4cf84c86c330c501fcd27b3e3193b6e1e905ddcf
Hadi Badjian <hadi@badjian.me>
2017-11-06 16:36:15 +1100

    Enable sshd and nginx jail configurations.

--- scripts/install-fail2ban.sh
+++ scripts/install-fail2ban.sh
@@ -1,3 +1,31 @@
 #!/usr/bin/env bash
 
+DESTEMAIL=user@example.com
+
+if [[ "$DESTEMAIL" == "user@example.com" ]]; then
+  echo 'Destination email not configured.'
+  exit
+fi
+
 sudo apt install -y fail2ban
+
+echo "
+[DEFAULT]
+bantime  = 3600
+
+destemail = $DESTEMAIL
+
+action = %(action_mwl)s
+
+[sshd]
+enabled = true
+
+[sshd-ddos]
+enabled = true
+
+[nginx-http-auth]
+enabled = true
+
+[nginx-botsearch]
+enabled = true
+" | sudo tee /etc/fail2ban/jail.local
