cce2665a7a4adb5d6e236101b904d7025708f9bc
Hadi Badjian <hadi@badjian.me>
2017-11-09 02:42:46 +1100

    Update ruby installation using rvm apt package.

--- scripts/install-ruby.sh
+++ scripts/install-ruby.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-VERSION=2.4.0
+VERSION=2.4.1
 
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-curl -sSL https://get.rvm.io | bash -s stable
-source ~/.rvm/scripts/rvm
+sudo apt-add-repository -y ppa:rael-gc/rvm
+sudo apt-get update
+sudo apt-get install rvm
+
 rvm install ${VERSION}
 rvm use ${VERSION} --default
 ruby -v
