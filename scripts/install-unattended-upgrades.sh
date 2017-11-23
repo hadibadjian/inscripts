04dc36096b27bf66e4fe80353697f10d8cca7b37
Hadi Badjian <hadi@badjian.me>
2017-11-24 03:22:16 +1100

    Enable automatic updates.

--- scripts/install-unattended-upgrades.sh
+++ scripts/install-unattended-upgrades.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
 sudo apt-get install -y unattended-upgrades
+
+echo "
+APT::Periodic::Update-Package-Lists \"1\";
+APT::Periodic::Download-Upgradeable-Packages \"1\";
+APT::Periodic::AutocleanInterval \"3\";
+APT::Periodic::Unattended-Upgrade \"1\";
+
+" | sudo tee /etc/apt/apt.conf.d/20auto-upgrades
+
+systemctl restart unattended-upgrades.service
