eebb4567e58389ccc10271f82e56ff091f5f0f48
Hadi Badjian <hadi@badjian.me>
2017-11-29 14:20:01 +1100

    Update fail2ban configuration.

--- scripts/install-fail2ban.sh
+++ scripts/install-fail2ban.sh
@@ -1,64 +1,8 @@
 #!/usr/bin/env bash
 
-DESTEMAIL=user@example.com
-
-if [[ "$DESTEMAIL" == "user@example.com" ]]; then
-  echo 'Destination email not configured.'
-  exit
-fi
-
 sudo apt install -y fail2ban
 
 # ddos filter script
-echo "
-[Definition]
-failregex = limiting requests, excess:.* by zone.*client: <HOST>
-ignoreregex =
+sudo cp fail2ban/filter.d/nginx-ddos.conf /etc/fail2ban/filter.d/nginx-ddos.conf
 
-" | sudo tee /etc/fail2ban/filter.d/nginx-ddos.conf
-
-# ddos action script
-echo "
-[Definition]
-actionstart =
-actionstop =
-actioncheck =
-actionban = IP=<ip> &&
-            printf %%b \"<daemon_list>: $IP\n\" >> <file>
-actionunban = echo \"/^<daemon_list>: <ip>$/<br>d<br>w<br>q\" | ed <file>
-
-[Init]
-file = /etc/hosts.deny
-daemon_list = ALL
-
-" | sudo tee /etc/fail2ban/action.d/hostsdeny.conf
-
-echo "
-[DEFAULT]
-bantime  = 3600
-
-destemail = $DESTEMAIL
-
-action = %(action_mwl)s
-
-[sshd]
-enabled = true
-
-[sshd-ddos]
-enabled = true
-
-[nginx-http-auth]
-enabled = true
-
-[nginx-botsearch]
-enabled = true
-
-[nginx-ddos]
-enabled   = true
-port      = http,https
-banaction = hostsdeny
-findtime  = 120
-bantime   = 7200
-maxretry  = 30
-logpath   = %(nginx_error_log)s
-" | sudo tee /etc/fail2ban/jail.local
+sudo cp fail2ban/jail.local /etc/fail2ban/jail.local
